f91dd86b98cc70a9bd589ae303f2354bc6cd6dbd
PabloBC <pabloboterocardona@gmail.com>
2026-08-27 13:32:14 +0200

    refactor: reorganizar importaciones en FormulasController para mejorar la legibilidad

--- src/main/java/com/pharmacyfm/api/controller/FormulasController.java
+++ src/main/java/com/pharmacyfm/api/controller/FormulasController.java
@@ -1,13 +1,22 @@
 package com.pharmacyfm.api.controller;
 
-import com.pharmacyfm.service.FormulaService;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.pharmacyfm.domain.model.Formula;
+import com.pharmacyfm.service.FormulaService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 /**
  * Endpoints REST para el catálogo de fórmulas magistrales.
